79f4965b4f4fed956ccabc68276d53b8f9db19aa
rvjansen <rvjansen>
2012-04-07 16:56:11 +0000

    added pg, boilerplate

---
+++ documentation/pg/auto/pg.el
@@ -0,0 +1,25 @@
+(TeX-add-style-hook "pg"
+ (lambda ()
+    (TeX-run-style-hooks
+     "rail"
+     "caption"
+     "listings"
+     "xcolor"
+     "color"
+     "lingmacros"
+     "xy"
+     "all"
+     "makeidx"
+     "booktabs"
+     "tabularx"
+     "fontspec"
+     "graphics"
+     "hyperref"
+     "latex2e"
+     "bk10"
+     "book"
+     "10pt"
+     "../boilerplate/netrexxformat"
+     "../boilerplate/series"
+     "../boilerplate/conventions")))
+
