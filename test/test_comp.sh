2a860107c4576e9a5ac9bd68fe04d1a8a91e7a8a
rvjansen@xs4all.nl <rvjansen@xs4all.nl>
2013-06-27 15:58:48 +0000

    move tests into trunk (1)

--- test/test_comp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/tcsh
-#without property
-java org.netrexx.process.NetRexxC ./hello.nrx -replace -keepasjava
-#with javac
-java -Dnrx.compiler=javac org.netrexx.process.NetRexxC ./hello.nrx -replace -keepasjava
-#with ecj
-java -Dnrx.compiler=ecj org.netrexx.process.NetRexxC ./hello.nrx -replace -keepasjava -diag
