3bdca02544b3d038f17b0301cbccc7cd8b22e6cd
rvjansen <rvjansen>
2013-07-23 07:31:37 +0000

    documentation

--- documentation/pg/auto/pg.el
+++ documentation/pg/auto/pg.el
@@ -1,14 +1,28 @@
 (TeX-add-style-hook "pg"
  (lambda ()
+    (LaTeX-add-index-entries
+     "compiling,from another program"
+     "completion codes, from translator"
+     "return codes, from translator"
+     "PrintWriter stream for capturing translator output"
+     "capturing translator output")
     (LaTeX-add-labels
      "interpreted"
      "classes"
+     "events"
      "editors")
     (TeX-run-style-hooks
      "../boilerplate/preamble"
      "../boilerplate/bookmeta"
      "../boilerplate/series"
      "../boilerplate/conventions"
+     "../railroad/loop"
+     "../ug/options"
      "nruappl"
-     "nruapi")))
+     "mqtt"
+     "nruapi"
+     "../railroad/java2nrx1"
+     "../railroad/java2nrx2"
+     "eclipseguide"
+     "resolution")))
 
