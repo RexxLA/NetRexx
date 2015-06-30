82f56b4b274f105107f1f804daee2d0704efdab0
rvjansen@xs4all.nl <rvjansen@xs4all.nl>
2015-06-30 11:22:11 +0000

    improved windows bat script

--- bin/NetRexxC.bat
+++ bin/NetRexxC.bat
@@ -31,28 +31,38 @@
 @REM : 2011.09.01 -- remove -xms4M
 @REM : 2011.09.29 -- add error msg for -run with x.nrx name format 
 @REM : 2014.05.30 -- default to ecj compiler and add NetRexxF.jar to classpath 
+@REM : 2015.06.30 -- remove operand limit, allow quotes, spaces and parens in classpath, add NetRexxC.jar to classpath if found
 
 @echo off
-set nrcpath=%CLASSPATH%;.
-if not "%nrcpath:NetRexxF.jar=%"=="%nrcpath%" goto setcomp
+set nrcpath=%CLASSPATH%;
+set nrcpath=%nrcpath:"=%
+set "nrcpath=%nrcpath: =?%"
+set "nrcpath=%nrcpath:(=^<%"
+set "nrcpath=%nrcpath:)=^>%"
+set "nrcpathf=%nrcpath:NetRexxF.jar=%"
+if not "%nrcpathf%"=="%nrcpath%" goto setcomp
 set binpath=%~dp0
 set libpath=%binpath:\bin=\lib%NetRexxF.jar
-if exist %libpath% (set nrcpath="%CLASSPATH%;.;%libpath%") else (goto compset)
+if exist %libpath% (set "nrcpath=%nrcpath%;%libpath%") else (goto compset)
 :setcomp
 if defined netrexx_java goto potluck
   set netrexx_java=-Dnrx.compiler=ecj
   goto potluck
 :compset
-set nrcpath=%CLASSPATH%;.
-if not "%nrcpath:NetRexxC.jar=%"=="%nrcpath%" goto potluck
+set "nrcpathc=%nrcpath:NetRexxC.jar=%"
+if not "%nrcpathc%"=="%nrcpath%" goto potluck
 set binpath=%~dp0
 set libpath=%binpath:\bin=\lib%NetRexxC.jar
-if exist %libpath% (set nrcpath="%CLASSPATH%;.;%libpath%")
+if exist %libpath% (set "nrcpath=%nrcpath%;%libpath%")
 :potluck
+set "nrcpath=%nrcpath:?= %"
+set "nrcpath=%nrcpath:^<=(%"
+set "nrcpath=%nrcpath:^>=)%"
 set netrexxc.bat_run=no
 if not '%1'=='-run' goto compile
   set netrexxc.bat_run=yes
   set netrexxc.runner=%2
+  set netrexxc.runner=%netrexxc.runner:.nrx=%
   shift
 :compile
 if not -%1-==-- goto maywanthelp
@@ -70,12 +80,12 @@ goto argactionstart
 :argactionend
 :docompile
 rem echo java -cp %nrcpath% %netrexx_java% org.netrexx.process.NetRexxC %1 %2 %3 %4 %5 %6 %7 %8 %9
-echo java -cp %nrcpath% %netrexx_java% org.netrexx.process.NetRexxC %nrcopts%
-java -cp %nrcpath% %netrexx_java% org.netrexx.process.NetRexxC %nrcopts%
+echo java -cp "%nrcpath%;." %netrexx_java% org.netrexx.process.NetRexxC %nrcopts%
+java -cp "%nrcpath%;." %netrexx_java% org.netrexx.process.NetRexxC %nrcopts%
 if errorlevel 2 goto quit
 if %netrexxc.bat_run%==no goto quit
 echo Running %netrexxc.runner% ...
 rem echo Running %1...
-IF EXIST %netrexxc.runner%.class (java -cp %nrcpath% %netrexxc.runner%) ELSE echo -run error: class file not found - do not add .nrx to name
+IF EXIST %netrexxc.runner%.class (java -cp "%nrcpath%;." %netrexxc.runner%) ELSE echo -run error: class file not found - do not add .nrx to name
 rem IF EXIST %1.class (java -cp %nrcpath% %1) ELSE echo -run error: class file not found - do not add .nrx to name
 :quit
